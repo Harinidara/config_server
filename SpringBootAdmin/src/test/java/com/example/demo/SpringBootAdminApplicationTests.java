0b24935a98eee8f04c97338756c80086fa95ded9
Harinidara <62381048+Harinidara@users.noreply.github.com>
2020-04-17 21:50:30 +0530

    commit

---
+++ SpringBootAdmin/src/test/java/com/example/demo/SpringBootAdminApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class SpringBootAdminApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
